3fefeec55c990ffb84c8b10452e15e464ce602a3
zhengtao.wuzt <zhengtao.wuzt@alibaba-inc.com>
2016-04-08 18:01:24 +0800

    add scripts

--- hadoop-dist/bin/create-app.sh
+++ hadoop-dist/bin/create-app.sh
@@ -1,3 +1,4 @@
-TARGET=$1
-#curl -d "" http://${TARGET}:8088/ws/v1/cluster/apps/new-application
+TARGET=`docker inspect -f '{{.NetworkSettings.Networks.vlan217.IPAddress}}' rm1`
+APPLICATION_ID=`curl -d "" http://${TARGET}:8088/ws/v1/cluster/apps/new-application | jq '."application-id"' `
+sed -i "s/application_id/${APPLICATION_ID}/g" commitapp.data
 curl -H "Accept: application/json" -H "Content-Type: application/json"  -d "@commitapp.data"   http://${TARGET}:8088/ws/v1/cluster/apps
